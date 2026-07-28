d6e0d98fa2d7f3750c4fe635f15eb47e97adc303
diyorbekjakbarov7-ctrl <diyorbekjakbarov7@gmail.com>
2026-07-28 05:39:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,6 +40,12 @@ def main():
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+    MessageHandler(
+        filters.Regex("^📦 Ombor$"),
+        show_stock,
+    )
+)
 
     conv_handler = ConversationHandler(
         entry_points=[
