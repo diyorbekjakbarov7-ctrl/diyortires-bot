ee065b36f02e26ce021484e3daa06ad026f53e6e
diyorbekjakbarov7-ctrl <diyorbekjakbarov7@gmail.com>
2026-07-29 00:09:42 +0500

    Update database.py

--- database.py
+++ database.py
@@ -115,3 +115,109 @@ def get_products():
     conn.close()
 
     return products
+    def sell_product(name, quantity):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM products WHERE name=?",
+        (name,)
+    )
+
+    product = cursor.fetchone()
+
+    if not product:
+        conn.close()
+        return False, "Mahsulot topilmadi."
+
+    if product["quantity"] < quantity:
+        conn.close()
+        return False, "Omborda yetarli mahsulot yo'q."
+
+    new_quantity = product["quantity"] - quantity
+
+    cursor.execute(
+        "UPDATE products SET quantity=? WHERE id=?",
+        (new_quantity, product["id"])
+    )
+
+    cursor.execute(
+        """
+        INSERT INTO history (product_id, action, quantity)
+        VALUES (?, ?, ?)
+        """,
+        (product["id"], "SELL", quantity)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return True, "Sotuv muvaffaqiyatli bajarildi."
+    def get_history():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT
+            h.action,
+            p.name,
+            h.quantity,
+            h.created_at
+        FROM history h
+        JOIN products p
+        ON p.id = h.product_id
+        ORDER BY h.id DESC
+    """)
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return data
+    def search_product(keyword):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT *
+        FROM products
+        WHERE name LIKE ?
+        ORDER BY name
+        """,
+        (f"%{keyword}%",)
+    )
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return data
+    def update_product(product_id, price, quantity):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        UPDATE products
+        SET
+            price=?,
+            quantity=?
+        WHERE id=?
+        """,
+        (price, quantity, product_id)
+    )
+
+    conn.commit()
+    conn.close()
+    def delete_product(product_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM products WHERE id=?",
+        (product_id,)
+    )
+
+    conn.commit()
+    conn.close()
